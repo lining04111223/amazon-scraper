b5a640c64889e172cb4034fe13d7341b5d86c75f
Ning Li <lining04111223@gmail.com>
2023-01-01 08:33:42 -0800

    fetch productID

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 import express from "express";
-import axios from "axios";
+
+import request from "request-promise";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const apiKey = "05052269c75c642ef3bb0827de249a13";
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+  try {
+    const response = await request(
+      `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true&url=http://www.amazon.com/dp/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 app.listen(PORT, () => console.log(`Serving is running on ${PORT}`));
